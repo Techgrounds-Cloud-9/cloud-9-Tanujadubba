0d3d31679dbfaf963e3639513e2bf87233ad368f
tanuja <tanuja.dubba@gmail.com>
2022-10-20 11:08:18 +0200

    bonus

---
+++ python/programfiles/4bonus.py
@@ -0,0 +1,5 @@
+
+foo = 0
+while foo < 5:
+	print('there are', foo, 'kids on the street')
+foo += 1
---
+++ python/programfiles/6bonus.py
@@ -0,0 +1,9 @@
+
+foo = 80
+if foo < 30:
+	print(foo)
+elif foo < 100:	
+	print(100)
+else:
+	print('big number wow')
+	print(foo)
